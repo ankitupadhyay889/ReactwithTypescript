efc710dc6e987a36b5da1e998c0efdb8fad1910b
Ankit <ankitup889@gmail.com>
2022-02-10 16:42:00 +0530

    Second commit

--- src/Component/Newgen.tsx
+++ src/Component/Newgen.tsx
@@ -6,10 +6,23 @@ type Cosa = {
 
 class Newgen extends Component<{} , Cosa> {
 
+    co = () => {
+        this.setState({time: new Date()})
+    }
+
+    // Before the component mounts, we initialise our state
+    componentWillMount() {
+        this.co();
+    }
+
+    componentDidMount() {
+        setInterval(() => this.co() , 1000)
+    }
+
     render() {
         return (
             <div>
-                <h1> {} </h1>
+                <h1>The current time is {this.state.time.toLocaleTimeString()} </h1>
             </div>
         );
     }
