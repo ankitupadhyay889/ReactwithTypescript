57e116a08ee96146fb988cc0e4b00221b9d8240f
Ankit <ankitup889@gmail.com>
2022-02-11 15:53:11 +0530

    error free

--- src/Component/Event.tsx
+++ src/Component/Event.tsx
@@ -22,8 +22,8 @@ class Event extends Component<Mpro , Msa> {
 
     hanCh = (e:React.ChangeEvent<HTMLInputElement>) => {
         this.setState({
-            name: e.target.value,
-            email: e.target.value
+            name: e.currentTarget.value,
+            email : e.currentTarget.value
         })
     }
 
@@ -35,13 +35,13 @@ class Event extends Component<Mpro , Msa> {
 
     render() {
         return (
-            <div>
-                <label>Employee Name: <input type="text" value={this.state.name} onChange={this.hanCh} /></label><br/>
-                <label>Employee Email: <input type="text" value={this.state.email} onChange={this.hanCh} /></label><br/>  
+            <form>
+                <label>Employee Name: <input type="text" name="name" value={this.state.name} onChange={this.hanCh} /></label><br/>
+                <label>Employee Email: <input type="text" name="email" value={this.state.email} onChange={this.hanCh} /></label><br/>  
                 <button onClick={this.creA}> Create </button> 
                 <h1> {this.state.name} </h1>
                 <h1> {this.state.email} </h1>           
-            </div>
+            </form>
         );
     }
 }
