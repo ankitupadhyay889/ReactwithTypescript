08424925e7bc730d20ae11c3ded642dc41c66e21
Ankit <ankitup889@gmail.com>
2022-02-14 16:40:25 +0530

    New user Commit

--- src/FuncCompo/state/Staecall.tsx
+++ src/FuncCompo/state/Staecall.tsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Staecall = () => {
+
+    const [log, setlog] = useState(false)
+
+    const li = () => {
+        setlog(true)
+    }
+
+    const lo = () => {
+        setlog(false)
+    }
+
     return (
         <div>
-            <h1> Use state </h1>
+            <button onClick={li}> LogIn </button>
+            <br/><br/>
+            <button onClick={lo}> LogOut </button>
+            <h1> User is {log ? `logged in` : 'logged out'} </h1>
         </div>
     );
 };
---
+++ src/FuncCompo/state/Userlog.tsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+
+type authU = {
+    name: string;
+    email: string;
+}
+
+const Userlog = () => {
+
+    const [user, setuser] = useState<authU | null> (null)
+
+    const li = () => {
+        setuser({
+            name: "ankit",
+            email: "ank@exam.com"
+        })
+    }
+
+    const lo = () => {
+        setuser(null)
+    }
+
+    return (
+        <div>
+            <button onClick={li}> LogIn </button>
+            <br/>
+            <button onClick={lo}> LogOut </button>
+            <br/>
+            <div> User name is {user?.name} </div>
+            <div> User email is {user?.email} </div>
+        </div>
+    );
+};
+
+export default Userlog;
