15ff88c22a0f32ba3b70dabb7cf5a29ca89c93c0
Ankit <ankitup889@gmail.com>
2022-02-15 18:05:50 +0530

    errore

---
+++ src/FuncCompo/state/Userregis.tsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+
+const Userregis = () => {
+
+    const [name, setname] = useState<Object<any>>({});
+    const [show, setshow] = useState<Array<any>>([])
+
+    const sub = (e:any) => {
+        e.preventDefault();
+        const all = {name:name};
+        setshow([...show , all]);
+        setname("");
+    }
+
+    const haC = (e:any) => {
+        setname(e.currentTarget.value)
+    }
+
+    return (
+        <div>
+            <form onSubmit={sub}>
+                <label> Enter name: </label> 
+                <input type="text" name="name" autoComplete="off" value={name} onChange={haC} />
+                <br/>
+                <button type="submit">Submit</button>
+            </form>
+            <div>
+                {
+                    show.map((curEl:any , id:number) => {
+                        return(
+                            <div key={id}>
+                                <p> {curEl.name} </p>
+                            </div>
+                        )
+                    })
+                }
+            </div>
+        </div>
+    );
+};
+
+export default Userregis;
