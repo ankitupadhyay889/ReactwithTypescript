013a576558bd72e676fc055d94552e0e147bde65
Ankit <ankitup889@gmail.com>
2022-02-16 15:36:52 +0530

    Few Update

--- src/Component/Child.tsx
+++ src/Component/Child.tsx
@@ -9,9 +9,10 @@ export default class Child extends Component<Adpro> {
   render() {
     return (
       <div>
-          <h1 style={this.props.styles} > Ankit Upadhyay </h1>
+          <h1 style={this.props.styles} > Ankit Upadhyay </h1> 
           <p> {this.props.children} </p>
       </div>
     )
   }
 }
+//ye jo hum call krte hai <Child> Ankigigi </Child> jo likhe iske andar iskko child khte hai or call k liye children prop use.
