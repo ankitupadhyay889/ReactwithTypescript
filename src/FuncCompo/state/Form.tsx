dba4dd537387995246ec01c6edd3dc8337ecc6b9
Ankit <ankitup889@gmail.com>
2022-02-17 13:23:01 +0530

    YupValidError

--- src/FuncCompo/state/Form.tsx
+++ src/FuncCompo/state/Form.tsx
@@ -4,8 +4,8 @@ import * as Yup from 'yup';
 
 const Form = () => {
 
-    const [data, setdata] = useState<any>(() => Yup.object().shape({
-        fullname: Yup.string().required('Fullname is required'),
+    const [data, setdata] = useState<string>(() => Yup.object().shape({
+        fullname: Yup.string().required("Fullname is required"),
         username: Yup.string().required('Username is required').min(6, 'Username must be at least 6 characters').max(20, 'Username must not exceed 20 characters'),
         email: Yup.string().required('Email is required').email('Email is invalid'),
     }));
