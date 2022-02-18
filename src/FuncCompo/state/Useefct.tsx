007085bd0b08e51b475333c66ad90c5e4a9e4914
Ankit <ankitup889@gmail.com>
2022-02-18 18:38:50 +0530

    useEffect

---
+++ src/FuncCompo/state/Useefct.tsx
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from 'react';
+
+const Useefct = () => {
+    
+    const [load, setload] = useState(true);
+    const [data, setdata] = useState<Array>>([]);
+
+    const getDa = async() => {
+        const res = await fetch('https://disease.sh/v3/covid-19/countries')  
+        const da = await res.json()  
+        setdata(da);
+        setload(false)    
+    }
+
+    useEffect(() => {
+        getDa();
+    }, [])
+
+    return (
+        <div>
+            {
+                load?<h1>Loading...</h1>:data.map((item,i)=>{
+                    <div> {item.country} </div>
+                })
+            }
+        </div>
+    );
+};
+
+
+export default Useefct;
