176efbfb0a7bf1f210bf776d4d4970e31fe71744
Ankit <ankitup889@gmail.com>
2022-02-24 18:11:55 +0530

    UesRefHookCommit

--- src/FuncCompo/state/UserefffForm.tsx
+++ src/FuncCompo/state/UserefffForm.tsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 
 const UserefffForm = () => {
+
+    const luname = useRef<HTMLInputElement>(null)
+
+    const [show, setshow] = useState(false);
+
+    const Op = (e: any) => {
+        e.preventDefault();
+        const sjo = luname.current?.value;
+        sjo === "" ? alert("please fill the data") : setshow(true);
+    }
+
     return (
         <div>
-            
+            <form onSubmit={Op}>
+                <div>
+                    <label htmlFor='name'> Enter your name </label>
+                    <input type="text" ref={luname} />
+                </div>
+                <br/>
+                <button type='submit'> Submit </button>
+            </form>
+            <p> {show ? `Your name is ${luname.current?.value}` : ""} </p>
         </div>
     );
 };
