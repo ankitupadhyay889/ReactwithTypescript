0be6133d27a18bd867456948c5435dede8d4ec7e
Ankit <ankitup889@gmail.com>
2022-03-08 11:56:13 +0530

    httpclientdonematerial

--- src/FuncCompo/state/ApiCall.tsx
+++ src/FuncCompo/state/ApiCall.tsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react';
 import axios,{AxiosResponse} from "axios";
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
-import Divider from '@mui/material/Divider';
 import ListItemText from '@mui/material/ListItemText';
 import ListItemAvatar from '@mui/material/ListItemAvatar';
 import Avatar from '@mui/material/Avatar';
---
+++ src/FuncCompo/state/HighComp.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HighComp = () => {
+    return (
+        <div>
+            <h1> High Order Component. </h1>
+        </div>
+    );
+};
+
+export default HighComp;
