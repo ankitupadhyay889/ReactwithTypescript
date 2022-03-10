b0642fdc07955c122cfd60cc66157244663b5e2c
Ankit <ankitup889@gmail.com>
2022-03-10 11:17:48 +0530

    cutominputerr

--- src/FuncCompo/custcompo/Inpcus.tsx
+++ src/FuncCompo/custcompo/Inpcus.tsx
@@ -1,22 +1,22 @@
-import React, { ChangeEvent, useState } from 'react';
+// import React, { useState } from 'react';
 
-type pp = {
-    onClick: (() => void);
-    values: React.InputHTMLAttributes<HTMLInputElement>;value?: string | number | readonly string[] | undefined
-}
+// type pp = {
+//     onClick: (() => void);
+//     values: React.InputHTMLAttributes<HTMLInputElement>;value?: string | number | readonly string[] | undefined
+// }
 
 const Inpcus = () => {
 
-    const [dt, setdt] = useState<pp>();
+    // const [dt, setdt] = useState<pp>();
 
-    const haC = (e: any) => {
-        setdt(e.currentTarget.value)
-    }
+    // const haC = (e: any) => {
+    //     setdt(e.currentTarget.value)
+    // }
 
     return (
         <div>
             <form>
-                <label> Enter name: </label> 
+                <label> Enter name:  </label> 
                 {/* <input type="text" name="name" value={dt} onChange={haC} /> */}
             </form>
         </div>
