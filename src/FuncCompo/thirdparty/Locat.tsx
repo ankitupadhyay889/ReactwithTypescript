311d96f19cd90a8b02aed6bbc0e6aef1395e25ca
Ankit <ankitup889@gmail.com>
2022-03-31 16:32:19 +0530

    googlemaperror

---
+++ src/FuncCompo/thirdparty/Locat.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Locat = (text:any) => {
+    return (
+        <div>
+            <p className="pin-text">{text}</p>
+        </div>
+    );
+};
+
+export default Locat;
